8ea7433e66cf9a6b7f63fdfe2a2c88298656d66c
emindlg <94062446+emindlg@users.noreply.github.com>
2022-06-01 14:39:35 +0300

    commit for about pages

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -14,20 +14,20 @@ const About = () => {
       <StyledImage src={codingSvg} />
       <HeaderContainer>
         <h1>
-          About Software Developer <span>AshleyMiller </span>
+          About Software Developer <span>Emin DELİGÖZ </span>
         </h1>
       </HeaderContainer>
       <InfoContainer>
-        <h2>Hi, I'am Ashley</h2>
+        <h2>Hi, I'am Emin</h2>
         <h3>I'm currently learning Full-Stack Development Languages.</h3>
         <h4>
           
-          I've already known JS, ReactJS, ReactNative, NodeJS, MongoDB,SQL,
-          Java, Hibernate, SpringBoot, AWS Services.
+          I've already known JS, ReactJS, Python, HTML-CSS, Bootstrap, MUI.
+          Still learning Django for backend.
         </h4>
         <h2>
           <a href="mailto:Ashley@clarusway.com">Send email</a> :
-          Ashley@clarusway.com
+          eminddeligoz@gmail.com
         </h2>
       </InfoContainer>
     </AboutContainer>
